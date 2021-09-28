8f334f4ae6361e53b2783779190363272def49ef
R5595 <irudhayaraj001@gmail.com>
2021-09-28 07:05:12 +0530

    sucess

---
+++ src/org/trg/Omega.java
@@ -0,0 +1,67 @@
+package org.trg;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+public class Omega {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		System.setProperty("webdriver.chrome.driver", "C:\\new selenum\\Selenium\\Driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.icc-cricket.com/rankings/mens/team-rankings/odi");
+		List<WebElement> list = driver.findElements(By.xpath("//table[@class='table']/tbody/tr/td[2]"));
+		List<WebElement> list1 = driver.findElements(By.xpath("//table[@class='table']/tbody/tr/td[3]"));
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getText().equalsIgnoreCase("Scotland")) {
+				System.out.println(list1.get(i).getText());
+			}
+			
+		}
+		
+
+		//driver.manage().window().maximize();
+		// WebElement drag = driver.findElement(By.id("email"));
+		// WebElement drop = driver.findElement(By.id("pass"));
+		// JavascriptExecutor js = (JavascriptExecutor)driver;
+		// js.executeScript("arguments[0].setAttribute('value','greens')", drag);
+		// String text = (String)js.executeScript("return
+		// arguments[0].getAttribute('value')", drag);
+		// System.out.println(text);
+		// js.executeScript("arguments[0].setAttribute('value','123')", drop);
+
+		// WebElement drag1= driver.findElement(By.xpath("(//a[@class='button
+		// button-orange'])[4]"));
+		// WebElement drop1 =
+		// driver.findElement(By.xpath("(//li[@class='placeholder'])[2]"));
+		//
+		//
+		// Actions a =new Actions(driver);
+		// a.dragAndDrop(drag, drop).perform();
+		// a.dragAndDrop(drag1, drop1).perform();
+		//
+		//
+		TakesScreenshot tk = (TakesScreenshot)driver;
+		 File file = tk.getScreenshotAs(OutputType.FILE);
+		// File f = new File("C:\\raju\\dr.png");
+		// FileUtils.copyFile(file, f);
+		 
+		 WebElement select = driver.findElement(By.id("jkbjbu"));
+		 Select s = new Select(select);
+		 List<WebElement> allSelectedOptions = s.getAllSelectedOptions();  
+		
+
+	}
+}
---
+++ src/org/trg/Trg.java
@@ -0,0 +1,35 @@
+package org.trg;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+public class Trg {
+public static void main(String[] args) throws InterruptedException {
+	System.setProperty("webdriver.chrome.driver", "C:\\new selenum\\Selenium\\Driver\\chromedriver.exe");
+	
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("http://www.greenstechnologys.com/");
+	
+	WebElement whatapp = driver.findElement(By.xpath("(//img[@title='Greens Technologys Whatsapp'])[1]"));
+//	Actions move = new Actions(driver);
+//	move.moveToElement(whatapp).perform();
+	whatapp.click();
+	
+	System.out.println(driver.getCurrentUrl());
+	
+	}
+}
